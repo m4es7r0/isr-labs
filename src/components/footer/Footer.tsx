73f651e2f9262ddf2cd42392992739a8b61ff7eb
Dmitry Musiichenko <41909013+m4es7r0@users.noreply.github.com>
2023-05-08 00:45:52 +0300

    feat: footer changes

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -20,35 +20,20 @@ const Footer: FC = () => {
               alt="telegram"
               className="h-7 w-7"
             />
-            <a href={`tel:${import.meta.env.VITE_CONTACT_PHONE}`}>
-              {import.meta.env.VITE_CONTACT_PHONE}
+            <a href={`${import.meta.env.VITE_CONTACT_PHONE}`} target="_blank">
+              Join us in telegram
             </a>
           </figure>
           <figure className="flex gap-3">
             <img src="/assets/letter.svg" alt="letter" className="h-7 w-7" />
-            <a href={`mailto:${import.meta.env.VITE_CONTACT_EMAIL}`}>
+            <a
+              href={`mailto:${import.meta.env.VITE_CONTACT_EMAIL}`}
+              target="_blank"
+            >
               {import.meta.env.VITE_CONTACT_EMAIL}
             </a>
           </figure>
         </div>
-
-        <ul>
-          <li>
-            <a>
-              <img src="/assets/letter-2.svg" alt="letter" />
-            </a>
-          </li>
-          <li>
-            <a>
-              <img src="/assets/telegram-2.svg" alt="telegram" />
-            </a>
-          </li>
-          <li>
-            <a>
-              <img src="/assets/card.svg" alt="bank card" />
-            </a>
-          </li>
-        </ul>
       </div>
     </footer>
   );
